6e7661a22416621f67de1f36cd01453f3da18ba8
tylerhedge <tylerhedge1@gmail.com>
2023-11-15 18:55:38 -0600

    added I gain, gravity, and the ability to block the controller

--- PIDdemo.py
+++ PIDdemo.py
@@ -1,12 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from math import copysign
 
 SP = 0 #setpoint
 velo = 5 #starting velocity
 accel = 0 #starting acceleration
-kp = 0.03
-kd = 0.009
+kp = 0.05
+kd = 0.02
+ki = 0.0001
+I = 0
+IMax = 2
+withIMax = True
+withBlockage = True
 kfriction = 0.003
+grav = -0.01
+withGrav = True
 elast = SP - velo #means kd wont freak out on first loop 
 times, dt = np.linspace(0, 100, 2500, retstep=True)
 y = []
@@ -16,7 +24,13 @@ for time in times:
     e = SP - velo
     de = (e - elast)/dt
     elast = e
-    accel += kp*e*dt + de*kd -accel*kfriction
+    accel += kp*e*dt + de*kd + ki*I -accel*kfriction
+    
+    I += e*dt
+    if withIMax: I = copysign(min(abs(I), IMax), I)
+    
+    if withGrav: accel += grav*dt
+    if withBlockage and time < 30: accel = 0
     velo+= accel*dt
     y.append(velo)
 
@@ -25,6 +39,6 @@ plt.plot(times, y, label='velocity', color='r')
 plt.plot((times[0], times[-1]), (SP, SP), label='setpoint', color='b')
 plt.xlabel('Time')
 plt.ylabel("Velocity")
-plt.title("Tuned PD controller")
+plt.title("PID controller reacting less violently with max I")
 plt.legend()
 plt.show()
